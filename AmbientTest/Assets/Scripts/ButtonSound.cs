59f8decf6630a9972ccec344bbbf073acffc0ff6
Colin Woodard <wooda012@cougars.csusm.edu>
2019-03-11 15:07:31 -0700

    'log

---
+++ AmbientTest/Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+	public AudioSource sound;
+	public int wait;
+	private int played;
+	public bool disableOnPlay;
+	private int enable;
+
+	void Start(){
+		sound = GetComponent<AudioSource>();
+		enable = 0;
+	}
+
+	void Update(){
+		if(played > 0){
+			played--;
+		}
+	}
+
+	void LookingAt(){
+		if(played == enable){
+		sound.Play();
+		played = wait;
+		}
+		if(disableOnPlay){
+			enable = -100;
+		}
+	}
+}
---
+++ AmbientTest/Assets/Scripts/FPController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPController : MonoBehaviour
+{
+	public float speed;
+
+
+	private Vector3 direction;
+	private Rigidbody rbody;
+
+	private float rotationSpeed = 1f;
+	private float minY = -60f;
+	private float maxY = 60f;
+	private float rotationY = 0f;
+	private float rotationX = 0f;
+
+    // Start is called before the first frame update
+	void Start()
+	{
+
+		speed = 5.0f;
+		rbody = GetComponent<Rigidbody>();
+
+	}
+
+    // Update is called once per frame
+	void Update()
+	{
+		direction = Vector3.zero;
+		direction.x = Input.GetAxis("Horizontal");
+		direction.z = Input.GetAxis("Vertical");
+		direction = direction.normalized;
+		if(direction.x != 0)
+		rbody.MovePosition(rbody.position + transform.right * direction.x * speed * Time.deltaTime);
+		if(direction.z != 0)
+		rbody.MovePosition(rbody.position + transform.forward * direction.z * speed * Time.deltaTime);
+
+		rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * rotationSpeed;
+		rotationY += Input.GetAxis("Mouse Y") * rotationSpeed;
+		rotationY = Mathf.Clamp(rotationY, minY, maxY);
+		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+	}
+}
